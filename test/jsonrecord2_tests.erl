c48255507c088f661ce58d21daf3c951a6d2e950
Eduard Sergeev <eduard.sergeev@gmail.com>
2012-07-12 23:37:47 +1000

    Clean up the tests slightly

--- test/jsonrecord2_tests.erl
+++ test/jsonrecord2_tests.erl
@@ -15,7 +15,7 @@
 
 -type my_integer() :: integer().
 
-%% -type my_list(A) :: [A].
+%%-type my_list(A) :: [A].
 
 
 -record(untyped_rec, {id, f1, f2}).
@@ -31,6 +31,7 @@
          rec = #rec0{id = 42} :: #rec0{},
          recs = [] :: [#rec0{}],
 %%         recs2 = [] :: my_list(#rec0{}),
+         recs2 = [] :: [#rec0{}],
          fi = <<>> :: binary()}).
 
 -type my_rec() :: #rec1{}.
@@ -55,93 +56,46 @@
 
 
 to_struct(Rec) when is_integer(Rec) ->
-    encode_gen(
-      meta:quote(Rec),
-      meta:reify_type(my_integer()),
-      meta:reify());
+    ?encode_gen(my_integer(), Rec);
 to_struct(Rec) when is_atom(Rec) ->
-    encode_gen(
-      meta:quote(Rec),
-      meta:reify_type(status()),
-      meta:reify());
+    ?encode_gen(status(), Rec);
 to_struct(#untyped_rec{} = Rec) ->
-    encode_gen(
-      meta:quote(Rec),
-      meta:reify_type(#untyped_rec{}),
-      meta:reify());
+    ?encode_gen(#untyped_rec{}, Rec);
 to_struct(#rec0{} = Rec) ->
-    encode_gen(
-      meta:quote(Rec),
-      meta:reify_type(#rec0{}),
-      meta:reify());
+    ?encode_gen(#rec0{}, Rec);
 to_struct(#rec1{} = Rec) ->
-    encode_gen(
-      meta:quote(Rec),
-      meta:reify_type(#rec1{}),
-      meta:reify());
+    ?encode_gen(#rec1{}, Rec);
 to_struct(#rec2{} = Rec) ->
-    encode_gen(
-      meta:quote(Rec),
-      meta:reify_type(#rec2{}),
-      meta:reify());
+    ?encode_gen(#rec2{}, Rec);
 to_struct(#rec3{} = Rec) ->
-    encode_gen(
-      meta:quote(Rec),
-      meta:reify_type(#rec3{}),
-      meta:reify());
+    ?encode_gen(#rec3{}, Rec);
 to_struct(#rec4{} = Rec) ->
-    encode_gen(
-      meta:quote(Rec),
-      meta:reify_type(#rec4{}),
-      meta:reify()).
+    ?encode_gen(#rec4{}, Rec).
 
 from_struct(integer, Struct) ->
-    decode_gen(
-      meta:quote(Struct),
-      meta:reify_type(my_integer()),
-      meta:reify());
+    ?decode_gen(my_integer(), Struct);
 from_struct(atom, Struct) ->
-    decode_gen(
-      meta:quote(Struct),
-      meta:reify_type(status()),
-      meta:reify());
+    ?decode_gen(status(), Struct);
 from_struct(untyped_rec, Struct) ->
-    decode_gen(
-      ?q(Struct),
-      meta:reify_type(#untyped_rec{}),
-      meta:reify());
+    ?decode_gen(#untyped_rec{}, Struct);
 from_struct(rec0, Struct) ->
-    decode_gen(
-      ?q(Struct),
-      meta:reify_type(#rec0{}),
-      meta:reify());
+    ?decode_gen(#rec0{}, Struct);
 from_struct(rec1, Struct) ->
-    decode_gen(
-      ?q(Struct),
-      meta:reify_type(#rec1{}),
-      meta:reify());
+    ?decode_gen(#rec1{}, Struct);
 from_struct(rec2, Struct) ->
-    decode_gen(
-      ?q(Struct),
-      meta:reify_type(#rec2{}),
-      meta:reify());
+    ?decode_gen(#rec2{}, Struct);
 %% from_struct(rec3, Struct) ->
-%%     decode_gen(
-%%       ?q(Struct),
-%%       meta:reify_type(#rec3{}),
-%%       meta:reify());
+%%    ?decode_gen(#rec3{}, Struct);
 from_struct(rec4, Struct) ->
-    decode_gen(
-      ?q(Struct),
-      meta:reify_type(#rec4{}),
-      meta:reify()).
+    ?decode_gen(#rec4{}, Struct).
 
 
 untyped_test() ->
-    Rec = #untyped_rec{id = 42, f1 = <<"Bin">>, f2 = atom},
-    Struct = to_struct(Rec),
-    Restored = from_struct(untyped_rec, Struct),
-    ?assertEqual(Rec, Restored).
+    Rec = #untyped_rec
+        {id = 42,
+         f1 = <<"Bin">>,
+         f2 = atom},
+    decode_encode(untyped_rec, Rec).
 
 
 rec0_test() ->
@@ -149,6 +103,19 @@ rec0_test() ->
         {id = 42,
          an = <<"Bin">>,
          atom = atom},
-    Struct = to_struct(Rec),
-    Restored = from_struct(rec0, Struct),
-    ?assertEqual(Rec, Restored).
+    decode_encode(rec0, Rec).
+
+rec1_test() ->
+    Rec = #rec1
+        {id = 1,
+         rec = #rec0{id = 2, an = 42.5},
+         recs = [#rec0{id = 3, an = false}, #rec0{id = 4, an = $#}],
+         fi = <<"La">>},
+    decode_encode(rec1, Rec).
+
+
+decode_encode(Tag, Item) ->
+    Struct = to_struct(Item),
+    Restored = from_struct(Tag, Struct),
+    ?assertEqual(Item, Restored).
+    
